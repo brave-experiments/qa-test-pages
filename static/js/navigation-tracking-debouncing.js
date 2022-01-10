2fe9492d55cf55342804e0555ab42ec5245056a8
Peter Snyder <snyderp@gmail.com>
2022-01-10 16:58:12 -0600

    add more debouncing tests

--- static/js/navigation-tracking-debouncing.js
+++ static/js/navigation-tracking-debouncing.js
@@ -3,22 +3,28 @@
   const D = W.document
   const BU = W.BRAVE
 
-  const startButtonElm = D.getElementById('start')
+  const testButtons = Array.from(D.querySelectorAll('.table button'))
   const referrerSectionElm = D.getElementById('observed-referer-section')
   const refererElm = D.getElementById('observed-referer')
 
   if (W.location.toString().includes('?complete') === true) {
     referrerSectionElm.classList.remove('d-none')
-    const refererHost = D.referrer || (new URL(D.referrer)).host
+    const refererHost = D.referrer || (new URL(D.referrer)).hostname
     refererElm.textContent = refererHost
   }
 
-  const onClick = _ => {
+  const onClick = (targetUrl, _) => {
     const finalUrl = BU.thisOriginUrlSecure('/navigation-tracking/debouncing.html?complete')
     const encodedUrl = W.encodeURIComponent(finalUrl)
-    const intermediateUrl = `https://www.youtube.com/redirect?q=${encodedUrl}`
+    const intermediateUrl = targetUrl + encodedUrl
     W.location = intermediateUrl
   }
-  startButtonElm.addEventListener('click', onClick, false)
-  startButtonElm.removeAttribute('disabled')
+
+  for (const aTestButton of testButtons) {
+    const rowElm = aTestButton.parentElement.parentElement
+    const testUrl = rowElm.dataset.testUrl
+    const testOnClick = onClick.bind(undefined, testUrl)
+    aTestButton.addEventListener('click', testOnClick, false)
+    aTestButton.removeAttribute('disabled')
+  }
 })()
