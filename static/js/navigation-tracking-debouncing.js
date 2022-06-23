ab488b60d07215531705bf36d381c116354590f0
Peter Snyder <snyderp@gmail.com>
2022-06-23 18:49:55 -0500

    update debouncing tests to only reference brave controlled servers

--- static/js/navigation-tracking-debouncing.js
+++ static/js/navigation-tracking-debouncing.js
@@ -1,7 +1,30 @@
 (_ => {
+  // https://github.com/joaquimserafim/base64-url/blob/master/index.js
+  function escape (str) {
+    return str.replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '')
+  }
+
+  function base64UrlEncode (str) {
+    return escape(window.btoa(str))
+  }
+
   const W = window
   const D = W.document
   const BU = W.BRAVE
+
+  const currentUrl = BU.thisOriginUrlSecure('/navigation-tracking/debouncing.html?complete')
+  const actionFunctionMapping = {
+    base64: bouncerUrl => bouncerUrl + base64UrlEncode(currentUrl),
+    regex: bouncerUrl => {
+      // The debouncing rule being tested does not want a protocol in place.
+      const currentUrlNoProto = currentUrl.replace('https://', '')
+      const encodedCurUrl = W.encodeURIComponent(currentUrlNoProto)
+      return bouncerUrl.replace('{replace}', encodedCurUrl)
+    },
+    none: bouncerUrl => bouncerUrl + W.encodeURIComponent(currentUrl)
+  }
 
   const testButtons = Array.from(D.querySelectorAll('.table button'))
   const referrerSectionElm = D.getElementById('observed-referer-section')
@@ -13,17 +36,16 @@
     refererElm.textContent = refererHost
   }
 
-  const onClick = (targetUrl, _) => {
-    const finalUrl = BU.thisOriginUrlSecure('/navigation-tracking/debouncing.html?complete')
-    const encodedUrl = W.encodeURIComponent(finalUrl)
-    const intermediateUrl = targetUrl + encodedUrl
-    W.location = intermediateUrl
+  const onClick = (bouncerUrl, transformFunc) => {
+    W.location = transformFunc(bouncerUrl)
   }
 
   for (const aTestButton of testButtons) {
     const rowElm = aTestButton.parentElement.parentElement
-    const testUrl = rowElm.dataset.testUrl
-    const testOnClick = onClick.bind(undefined, testUrl)
+    const bouncerUrl = BU.otherOriginUrl(rowElm.dataset.testUrl)
+    const testAction = rowElm.dataset.testAction || 'none'
+    const transformFunc = actionFunctionMapping[testAction]
+    const testOnClick = onClick.bind(undefined, bouncerUrl, transformFunc)
     aTestButton.addEventListener('click', testOnClick, false)
     aTestButton.removeAttribute('disabled')
   }
